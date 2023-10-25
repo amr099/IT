61598e29470e1d34cb0d5e9478720a1071c0372b
Amr <amr70210@gamil.com>
2023-10-25 17:00:05 +0300

    fix

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ $(()=>{
     for(let key of keys){
         $(`.${key}`).on('click',()=>{
             $(`.accord.${key}`).addClass('active')
-            $(`#findout div:not(.accord.${key})`).removeClass('active')
+            $(`.findout div:not(.accord.${key})`).removeClass('active')
 
-            $(`#findout figure:not(.${key})`).addClass('hide')
+            $(`.findout figure:not(.${key})`).addClass('hide')
             $(`.figure-${key}`).removeClass('hide')
             $(`.figure-${key}`).addClass('flex')
         })
